316fb1c627a87b8a29933e05235875f03df77153
starguys <kjj9500@naver.com>
2023-03-30 08:57:58 +0900

    ✨ layout에 title 추가

--- lib/common/layout/layout.dart
+++ lib/common/layout/layout.dart
@@ -3,24 +3,25 @@ import 'package:go_router/go_router.dart';
 
 class LayOut extends StatelessWidget {
   const LayOut(
-      {super.key, required this.child, this.appBar, this.isCanBack = false});
+      {super.key, required this.child, this.title, this.isCanBack = false});
 
   final Widget child;
-  final AppBar? appBar;
+  final String? title;
   final bool isCanBack;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: isCanBack ? renderAppBar(context) : appBar,
+      appBar: isCanBack ? renderAppBar(context, title) : null,
       body: child,
     );
   }
 }
 
-AppBar renderAppBar(BuildContext context) {
+AppBar renderAppBar(BuildContext context, String? title) {
   return AppBar(
     automaticallyImplyLeading: false,
+    title: Text(title ?? ''),
     leading: InkWell(
       borderRadius: BorderRadius.circular(100),
       onTap: () {
